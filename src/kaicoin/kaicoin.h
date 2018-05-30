14f7c73533f2362417bc62e27807f9c0db1fcb16
sungjoon.kim <methodtweak@naver.com>
2018-05-30 14:30:45 +0900

    initial push

---
+++ src/kaicoin/kaicoin.h
@@ -0,0 +1,22 @@
+// Copyright (c) 2014-2017 Coin Sciences Ltd
+// MultiChain code distributed under the GPLv3 license, see COPYING file.
+
+#ifndef KAICOIN_H
+#define	KAICOIN_H
+
+
+#include "utils/define.h"
+#include "utils/declare.h"
+    
+#include "chainparams/chainparams.h"
+#include "protocol/kaicoinscript.h"
+#include "permissions/permission.h"
+#include "entities/asset.h"
+#include "chainparams/state.h"
+
+    
+extern unsigned int MIN_RELAY_TX_FEE;  
+
+
+#endif	/* KAICOIN_H */
+
